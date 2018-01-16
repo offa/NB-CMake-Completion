214cddb15c2d1e82061195806cf9309f3fa21256
offa <bm-dev@yandex.com>
2018-01-16 11:20:11 +0100

    CI moved to docker.

---
+++ script/ci_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -ex
+
+# Workaround for broken JDK9 (see #20)
+if java -fullversion 2>&1 | grep -q '"9.';
+then
+    wget https://circle-downloads.s3.amazonaws.com/circleci-images/cache/linux-amd64/openjdk-9-slim-cacerts \
+        -O /etc/ssl/certs/java/cacerts
+fi
+
+
+# Install Dependencies
+mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+
+# Build and Test
+mvn test -B
+
